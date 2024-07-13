3389d3a2a81459520283175cea87f67d3ac08153
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-07-13 23:54:39 +0100

    did suff

---
+++ script.js
@@ -0,0 +1,13 @@
+let fullnameInput = document.getElementById('fullname');
+let emailInput = document.getElementById('email');
+
+function doClick(){
+
+    var name = fullnameInput.value
+    var email = emailInput.value
+
+    if (email.toString().trim().length === "" || name.toString().trim().length === ""){
+
+    }
+
+}
