bfe3c6777169e858d1f0872e585d9005199a4836
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-07-14 03:08:25 +0100

    done

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let fullnameInput = document.getElementById('fullname');
+let fullnameInput = document.getElementById("fullname");
 let emailInput = document.getElementById('email');
+let button = document.getElementById('button');
+let text = document.getElementById('text');
 
-function doClick(){
-
-    var name = fullnameInput.value
+function doClick() {
+    console.log("fffffffffffffffffffffffffffffffff")
+    name = fullnameInput.value
     var email = emailInput.value
 
-    if (email.toString().trim().length === "" || name.toString().trim().length === ""){
-
+    if (name.trim() === "") {
+        // document.getElementById("errorname").innerHTML = "Name must be filled out"
+        // fullnameInput.request.
+        alert("Name must be filled out");
     }
+
+    if (email.trim() === "") {
+        alert("Email must be filled out");
+    }
+
+    fullnameInput.hidden = true
+    emailInput.hidden = true
+    button.hidden = true
+
+    var newText = "Hello " + name + " thank you for signing up. A verification link has been sent to " + email
+    text.innerHTML = newText;
 
 }
